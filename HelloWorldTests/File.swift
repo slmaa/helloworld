6680eb738401fe9c96f9bf92a26407de63b22640
Salma <salma@salmas-macbook-air.local>
2016-01-02 22:46:23 +0400

    this is my first commit

---
+++ HelloWorldTests/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  HelloWorld
+//
+//  Created by Salma on 1/2/16.
+//  Copyright © 2016 Salma. All rights reserved.
+//
+
+import Foundation
+class person {
+    
+    
+    var name : String;
+    init(name:String){
+        self.name=name;
+    }
+}
